6b5979e46336e5ef09719933689001a75b49fa75
Chester <lillian.holl@kearsarge.org>
2025-01-14 08:30:28 -0500

    Setting up

--- Hud.py
+++ Hud.py
@@ -2,5 +2,14 @@ import pygame, sys, math
 
 class Hud():
     def __init__ (self,baseText, startPos=[0,0]):
-    
-    
+        self.font = pygame.font.Font(None, 46)
+        self.baseText = baseText
+        self.image = self.font.render("Score: 0", 1, (255,255,255))
+        self.rect = self.image.get_rect(topleft = startPos)
+        
+        
+        
+        def update(self, score):
+            text = self.baseText + str(score)
+            self.image =  self.font.render(text, 1, (255,255,255))
+            self.rect = self.image.get_rect(topleft = self.rect.topleft)
--- Main.py
+++ Main.py
@@ -1,6 +1,47 @@
 import pygame, sys, math, random
+from Hud import *
+from Pellet import *
 
 
+if not pygame.font: print("Warning, fonts disabled")
 
 size = [1000,900]
-screen = pygame.diplay.set _mode(size)
+screen = pygame.display.set_mode(size)
+
+clock = pygame.time.Clock();
+
+score = Hud ("Score: ",[0,0])
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit();
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame. K_a or event.key == pygame.K_LEFT:
+                player.goKey("left")
+            elif event.key == pygame. K_d or event.key == pygame.K_RIGHT:
+                player.goKey("right")
+            elif event.key == pygame. K_w or event.key == pygame.K_UP:
+                player.goKey("up")
+            elif event.key == pygame. K_s or event.key == pygame.K_DOWN:
+                player.goKey("down")
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame. K_a or event.key == pygame.K_LEFT:
+                player.goKey("sleft")
+            elif event.key == pygame. K_d or event.key == pygame.K_RIGHT:
+                player.goKey("sright")
+            elif event.key == pygame. K_w or event.key == pygame.K_UP:
+                player.goKey("sup")
+            elif event.key == pygame. K_s or event.key == pygame.K_DOWN:
+                player.goKey("sdown")
+
+
+
+
+    screen.fill([30,40,50])
+    screen.blit(score.image, score.rect)
+    screen.blit(timer.image, timer.rect)
+    pygame.display.flip()
+    clock.tick(60)
+    # print(clock.get_fps())
