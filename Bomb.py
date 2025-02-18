1b0e8d1cda5a0a1cd8269ebcf9a42c88625fab82
levi <levi.grady@kearsarge.org>
2025-02-18 08:51:14 -0500

    bomb moves now

--- Bomb.py
+++ Bomb.py
@@ -1,4 +1,4 @@
-import pygame, sys,math
+import pygame, sys,math,random
 
 class Bomb():
     def __init__(self, startPos=[0,9]):
@@ -72,3 +72,36 @@ class Bomb():
     
     def hit(self):
         print("'hit'")
+   
+   
+    
+        self.kind = "Bomb"
+    
+        
+    def snakeCollide(self, other):
+        if self != other:
+            if self.rect.right > other.rect.left:
+                if self.rect.left < other.rect.right:
+                    if self.rect.bottom > other.rect.top:
+                        if self.rect.top < other.rect.bottom:
+                            if self.getDist(other) < self.rad + other.rad:
+                                
+                                return True
+        return False
+                         
+                         
+                         
+    
+    def getDist(self, other):
+        x1 = self.rect.centerx
+        x2 = other.rect.centerx
+        y1 = self.rect.centery
+        y2 = other.rect.centery
+        return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+        
+        
+    def respawn(self, size, tileSize):
+        x =  random.randint(0, int(size[0]/tileSize)-1)*50+25
+        y =  random.randint(0, int(size[1]/tileSize)-1)*50+25
+        self.rect.center = [x,y]
+        print(x,y)
