382dd62ad749cb54345d88b7974d8a8f6303a160
mackenna1 <mackenna.rines@kearsarge.org>
2025-04-15 08:17:40 -0400

    audio

--- Main.py
+++ Main.py
@@ -45,7 +45,7 @@ while True:
     if sound:
         pygame.mixer.music.load("Music/Background/Main_Theme.mp3")
         pygame.mixer.music.set_volume(.25)
-        pygame.mixer.music.play()
+        pygame.mixer.music.play(0)
     else:
         print("No Sound")
     
